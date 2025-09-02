b70d01567b26f6ee9717dcdd082931ee8f9a146a
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2025-09-02 22:23:05 +0100

    2.1

--- Part 2/courseinfopart2/src/App.jsx
+++ Part 2/courseinfopart2/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Header = (props) => {
+    console.log(props)
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+        // i try to rename everything
+    )
 }
+const Part = (props) => {
+    return (
+        <>
+            <p>
+                {props.part} {props.exercises}
+            </p>
+        </>
+    )
+}
+const Content = ({parts}) => {
+    return (
+        <>
+            {parts.map((part, i) =>
+                <Part part={part.name} exercises={part.exercises} key={i} />
+            )}
+            {/*<Part part={parts[0].name} exercises={parts[0].exercises} />*/}
+            {/*<Part part={parts[1].name} exercises={parts[1].exercises} />*/}
+            {/*<Part part={parts[2].name} exercises={parts[2].exercises} />*/}
+
+        </>
+
+    )
+}
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+
+    )
+}
+const Course = ({course}) => {
+    return (
+        <>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </>
+
+    )
+}
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
+    }
+
+
+
+    return (
+        <div>
+            {/*<Header course={course.name} />*/}
+            {/*<Content parts={course.parts} />*/}
+            {/*<Total parts={course.parts} />*/}
+            <Course course={course} />
+        </div>
+    )
+}
+
+
 
 export default App
