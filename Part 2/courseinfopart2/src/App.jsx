eb7db53a6c228e7c768f6968fc7c1b3b329ae94e
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2025-09-02 23:09:34 +0100

    nearly done 2.4 but keep getting children don't have unique keys error

--- Part 2/courseinfopart2/src/App.jsx
+++ Part 2/courseinfopart2/src/App.jsx
@@ -1,4 +1,4 @@
-import {jasmine} from "globals";
+
 
 const Header = (props) => {
     console.log(props)
@@ -9,21 +9,23 @@ const Header = (props) => {
         // i try to rename everything
     )
 }
-const Part = (props) => {
+const Part = ({part, exercises}) => {
     return (
         <>
             <p>
-                {props.part} {props.exercises}
+                {part} {exercises}
             </p>
         </>
     )
 }
 const Content = ({parts}) => {
+    console.log(parts)
     return (
         <>
-            {parts.map((part, i) =>
-                <Part part={part.name} exercises={part.exercises} key={i} />
+            {parts.map((part) =>
+                <Part part={part.name} exercises={part.exercises} key={part.id} />
             )}
+
             {/*<Part part={parts[0].name} exercises={parts[0].exercises} />*/}
             {/*<Part part={parts[1].name} exercises={parts[1].exercises} />*/}
             {/*<Part part={parts[2].name} exercises={parts[2].exercises} />*/}
@@ -46,43 +48,73 @@ const Total = ({parts}) => {
 
     )
 }
-const Course = ({course}) => {
+const Course = ({course, parts}) => {
     return (
         <>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <Header key='1' course={course} />
+            <Content key='2' parts={parts} />
+            <Total key='3' parts={parts} />
+        </>
+        )
+}
+const Courses = ({courses}) => {
+    // courses.forEach((course) => console.log(course.id))
+    return (
+        <>
+            {courses.map((course) =>
+                <>
+                    <Course key={course.id} course={course.name} parts={course.parts} />
+                </>
+            )}
         </>
 
     )
 }
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 4
-            }
-        ]
-    }
-
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
 
     return (
@@ -90,7 +122,7 @@ const App = () => {
             {/*<Header course={course.name} />*/}
             {/*<Content parts={course.parts} />*/}
             {/*<Total parts={course.parts} />*/}
-            <Course course={course} />
+            <Courses courses={courses} />
         </div>
     )
 }
