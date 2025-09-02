f01a89ae89d62bf8e816c5af18372ef9b9ee5def
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2025-09-02 23:31:49 +0100

    2.5 fr

--- Part 2/courseinfopart2/src/App.jsx
+++ Part 2/courseinfopart2/src/App.jsx
@@ -1,66 +1,7 @@
-const Header = (props) => {
-    console.log(props)
-    return (
-        <>
-            <h1>{props.course}</h1>
-        </>
-        // i try to rename everything
-    )
-}
-const Part = ({part, exercises}) => {
-    return (
-        <>
-            <p>
-                {part} {exercises}
-            </p>
-        </>
-    )
-}
-const Content = ({parts}) => {
-    console.log(parts)
-    return (
-        <>
-            {parts.map((part) =>
-                <Part part={part.name} exercises={part.exercises} key={part.id} />
-            )}
+import Courses from './components/Courses.jsx'
 
-        </>
 
-    )
-}
-const Total = ({parts}) => {
-    const total =
-        parts.reduce(
-            (accumulator, currentValue) => accumulator + currentValue.exercises,
-            0,
-            )
-    return (
-        <>
-            <p><b>Number of exercises {total}</b></p>
-        </>
 
-    )
-}
-const Course = ({course, parts}) => {
-    return (
-        <>
-            <Header key='1' course={course} />
-            <Content key='2' parts={parts} />
-            <Total key='3' parts={parts} />
-        </>
-        )
-}
-const Courses = ({courses}) => {
-    return (
-        <>
-            {courses.map((course) =>
-                    <Course key={course.id} course={course.name} parts={course.parts} />
-
-            )}
-        </>
-
-    )
-}
 const App = () => {
     const courses = [
         {
@@ -118,3 +59,4 @@ const App = () => {
 
 
 export default App
+
